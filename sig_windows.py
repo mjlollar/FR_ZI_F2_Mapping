6761a8ee2fc886d6c18cce2750a1918971d00ba0
Matthew Lollar <mjlollar1@gmail.com>
2020-03-11 15:33:44 -0500

    Update sig_windows.py

--- sig_windows.py
+++ sig_windows.py
@@ -1,11 +1,10 @@
 #Script to filter out window comparisons that have lower p-values than null p-value
-#Adjust row 16 to appropriate p-value
 
 import sys
 import csv
 
 in_name = sys.argv[1] #Input file containing three columns [window1, window2, p-value]
-out_name = sys.argv[2]
+out_name = sys.argv[2] #File name for output 
 
 with open(in_name, 'r') as infile, open(out_name, 'w') as outfile:
 	reader = csv.DictReader(infile)
@@ -13,10 +12,7 @@ with open(in_name, 'r') as infile, open(out_name, 'w') as outfile:
 	writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter=',')
 	writer.writeheader()
 	for row in reader:
-		if row['p-value'] <= '0.00009': # ADAPT to null p-value of choice
+		if row['p-value'] <= '0.0003797': # ADAPT to null p-value of choice
 			writer.writerow(row['window1'], row['window2'], row['p-value'])
 		else:
 			pass
-
-writer.close()
-reader.close()
