778163de465cb857ffaee41425c376f5f8feacb1
Matthew Lollar <mjlollar1@gmail.com>
2020-06-08 11:46:43 -0500

    Update BD_test.R

--- BD_test.R
+++ BD_test.R
@@ -1,12 +1,11 @@
 #### R Script to calculate Breslow-Day Statistics from sterility outputs
 #### Accepts output in the form of a csv file with 8 rows and n columns
 #### Rows correspond to F1,F2,F3,...,F8 for 2x2x2 table where F1-F4 in 2x2 and F5-F8 in 2x2
-#### BRESLOW DAY TEST WILL NOT WORK IF dataframe[0,0] has a zero value. Placehold with integer to run
 
 library("DescTools")
 library("data.table")
 
-DT <- fread("sterility_BD_output_0to2.csv", header=FALSE, sep=',') ## Adjust for correct output file name
+DT <- fread("####", header=FALSE, sep=',')
 df <- setDF(DT)
 win_num <- ncol(df)
 p_values <- vector(length=win_num)
@@ -21,4 +20,7 @@ for (x in 1:win_num) {
 	rm(counts)
 }
 
-write.csv(p_values, "BD__final_output_sterility.csv", row.names=FALSE)
+out_ps <- as.data.frame(p_values)
+
+fwrite(out_ps, "####", sep=",", row.names=FALSE)
+
